09617f4de65e727fbf4b552c51dda9eb52727972
SelamT <selamt4709@gmail.com>
2024-06-15 19:54:38 +0300

    Creating database schema for order

---
+++ schema/orders.sql
@@ -0,0 +1,9 @@
+CREATE TABLE IF NOT EXISTS `Orders"
+(
+    `Trip ID` INT,
+    `Trip Start Time` TIMESTAMP,
+    `Trip End Time` TIMESTAMP,
+    `Trip Origin` TEXT,
+    `Trip Destination` TEXT,
+    PRIMARY KEY (`Trip ID`)
+)
